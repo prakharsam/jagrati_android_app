9a09501bef33fb7ff18d81d2bbb7aff6c9779209
Prakhar Sambhar <prakharsam97@gmail.com>
2018-02-14 01:15:28 +0530

    functional

--- app/src/main/java/com/example/lenovopc/jagrati/MainActivity.java
+++ app/src/main/java/com/example/lenovopc/jagrati/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.lenovopc.jagrati;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button attendance = (Button) findViewById(R.id.button);
 
     }
 }
