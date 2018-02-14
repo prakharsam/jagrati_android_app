e9472ce0b13acc67e9144e9bffb8cf72e6a24b8c
Sachin Kukreja <skad5455@gmail.com>
2018-02-15 03:49:51 +0530

    Removed unused imports

--- app/src/main/java/com/example/lenovopc/jagrati/MainActivity.java
+++ app/src/main/java/com/example/lenovopc/jagrati/MainActivity.java
@@ -2,11 +2,6 @@ package com.example.lenovopc.jagrati;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
-import android.widget.GridLayout;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
