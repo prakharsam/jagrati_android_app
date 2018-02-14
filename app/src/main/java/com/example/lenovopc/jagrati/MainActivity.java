bb6ccca837d9ec8f9be07a934509dc199838db7b
Rajeev Kumar <rajeeev001@gmail.com>
2018-02-15 02:01:49 +0530

    Restructured dynamic xml subject_button

--- app/src/main/java/com/example/lenovopc/jagrati/MainActivity.java
+++ app/src/main/java/com/example/lenovopc/jagrati/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.lenovopc.jagrati;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_home);
 
     }
 }
