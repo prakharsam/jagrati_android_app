aa3a495c4c76b1d305d68290726c42a4bb618854
Sachin Kukreja <skad5455@gmail.com>
2018-02-25 02:01:24 +0530

    Integrated class history with api

--- app/src/main/java/com/example/lenovopc/jagrati/History.java
+++ app/src/main/java/com/example/lenovopc/jagrati/History.java
@@ -1,11 +1,22 @@
 package com.example.lenovopc.jagrati;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class History extends BaseActivity {
 
@@ -13,8 +24,76 @@ public class History extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        Bundle bundle = getIntent().getExtras();
 
-        setBackOnClickListener();
-        setPageTitle("History");
+        if (bundle != null) {
+            setBackOnClickListener();
+            setPageTitle("History");
+
+            String classId = bundle.getString("classId");
+            getClassHistory(classId);
+        }
+    }
+
+    private void getClassHistory(String classId) {
+        final String historyURL = apiURL + "/class_feedback/?_class=" + classId;
+
+        JsonArrayRequest req = new JsonArrayRequest(
+                historyURL,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("@@@@@", response.toString());
+                        initializeClassHistory(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error", new String(error.networkResponse.data));
+                        error.printStackTrace();
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "JWT " + jwtVal);
+                return headers;
+            }
+        };
+
+        queue.add(req);
+    }
+
+    private void initializeClassHistory(JSONArray history) {
+        LinearLayout historyListLayout = (LinearLayout) findViewById(R.id.historyList);
+
+        for (int i=0; i < history.length(); i++) {
+            try {
+                JSONObject historyObj = history.getJSONObject(i);
+                JSONObject subject = historyObj.getJSONObject("subject");
+                String subjectName = subject.getString("name");
+                String feedback = historyObj.getString("feedback");
+                // TODO: Format the date value
+                String createdAt = historyObj.getString("created_at");
+
+                View historyBlockView = getLayoutInflater().inflate(R.layout.history_block, null);
+
+                TextView subjectView = (TextView) historyBlockView.findViewById(R.id.subject);
+                subjectView.setText(subjectName);
+
+                TextView feedbackView = (TextView) historyBlockView.findViewById(R.id.feedback);
+                feedbackView.setText(feedback);
+
+                TextView createdAtView = (TextView) historyBlockView.findViewById(R.id.createdAt);
+                createdAtView.setText(createdAt);
+
+                historyListLayout.addView(historyBlockView);
+            } catch (JSONException e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+        }
     }
 }
