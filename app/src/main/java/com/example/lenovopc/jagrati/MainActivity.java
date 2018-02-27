ae4aa79feb06049dcc1bb69c55a3cd9c24451cbc
Sachin Kukreja <skad5455@gmail.com>
2018-02-27 18:01:15 +0530

    Added functionality to tap twice to exit

--- app/src/main/java/com/example/lenovopc/jagrati/MainActivity.java
+++ app/src/main/java/com/example/lenovopc/jagrati/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.lenovopc.jagrati;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends BaseActivity {
     @Override
@@ -47,5 +49,26 @@ public class MainActivity extends BaseActivity {
                 startActivity(classActivity);
             }
         });
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click back again to exit.", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
