60fe6773c0a212208018df83dc0bc5daafc2877c
Sachin Kukreja <skad5455@gmail.com>
2018-05-11 18:39:21 +0530

    Added functionality to add subject

---
+++ app/src/main/java/com/example/lenovopc/jagrati/AddSubject.java
@@ -0,0 +1,103 @@
+package com.example.lenovopc.jagrati;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddSubject extends BaseActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_subject);
+        setBackOnClickListener();
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String header = bundle.getString("header");
+            final String urlSlug = bundle.getString("urlSlug");
+            setPageTitle(header);
+
+            Button formSubmitBtn = (Button) findViewById(R.id.formSubmitBtn);
+            formSubmitBtn.setText(header);
+            formSubmitBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    submitForm(urlSlug);
+                }
+            });
+        }
+    }
+
+    private void submitForm(String urlSlug) {
+        EditText nameView = (EditText) findViewById(R.id.name);
+        final String name = String.valueOf(nameView.getText());
+
+        if (!name.equals("")) {
+            String addSubjectURL = apiURL + urlSlug;
+
+            JSONObject formData = new JSONObject();
+            try {
+                formData.put("name", name);
+            } catch (JSONException e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+
+            final RelativeLayout formWrap = (RelativeLayout) findViewById(R.id.addSubjectForm);
+
+            showProgress(true, formWrap);
+            JsonObjectRequest req = new JsonObjectRequest(
+                    Request.Method.POST,
+                    addSubjectURL,
+                    formData,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            showProgress(false, formWrap);
+                            Toast.makeText(AddSubject.this, "Added successfully",
+                                    Toast.LENGTH_SHORT);
+                            try {
+                                Intent data = new Intent();
+                                data.putExtra("name", name);
+                                data.putExtra("id", response.getString("id"));
+                                setResult(RESULT_OK, data);
+                                finish();
+                            } catch (JSONException e) {
+                                Log.e("Error", e.getMessage());
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    getErrorListener(formWrap)
+            ) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<>();
+                    headers.put("Authorization", "JWT " + jwtVal);
+                    return headers;
+                }
+            };
+
+            queue.add(req);
+        } else {
+            nameView.setError("This field is required.");
+            nameView.requestFocus();
+        }
+    }
+}
--- app/src/main/java/com/example/lenovopc/jagrati/TeachingDepartment.java
+++ app/src/main/java/com/example/lenovopc/jagrati/TeachingDepartment.java
@@ -50,6 +50,19 @@ public class TeachingDepartment extends BaseActivity {
                 onPopupButtonClick(optionBtn);
             }
         });
+
+        Button addSubjectBtn = (Button) findViewById(R.id.addSubject);
+        addSubjectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addSubjectActivity = new Intent(TeachingDepartment.this, AddSubject.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("header", "Add Subject");
+                bundle.putString("urlSlug", "/subjects/");
+                addSubjectActivity.putExtras(bundle);
+                startActivityForResult(addSubjectActivity, 100);
+            }
+        });
     }
 
     protected void onPopupButtonClick(View button) {
@@ -104,32 +117,51 @@ public class TeachingDepartment extends BaseActivity {
                 GridLayout gridLayout = (GridLayout) findViewById(R.id.gridWrap);
                 View subjectButtonView = getLayoutInflater().inflate(R.layout.subject_button, null);
 
-                Button subjectNameBtn = (Button) subjectButtonView.findViewById(R.id.buttonTitle);
-                subjectNameBtn.setText(name);
-                subjectNameBtn.setOnClickListener( new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Class cls = SubjectTeachingDepartment.class;
-                        if (forAttendance) {
-                            cls = VolunteerAttendance.class;
-                        }
-                        Intent subjectDeptActivity = new Intent(TeachingDepartment.this, cls);
-                        Bundle bundle = new Bundle();
-                        bundle.putString("subjectId", id);
-                        bundle.putString("subjectName", name);
-                        subjectDeptActivity.putExtras(bundle);
-                        startActivity(subjectDeptActivity);
-                    }
-                });
-
-                TextView numTeachersTextView = (TextView) subjectButtonView.findViewById(R.id.buttonCaption);
-                numTeachersTextView.setText(numVolunteersLabel);
-
-                gridLayout.addView(subjectButtonView);
+                initializeSubject(gridLayout, subjectButtonView, id, name, numVolunteersLabel);
             } catch (JSONException e) {
                 Log.e("Error", e.getMessage());
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void initializeSubject(GridLayout gridLayout, View subjectButtonView, final String id,
+                                   final String name, final String numVolunteersLabel) {
+        Button subjectNameBtn = (Button) subjectButtonView.findViewById(R.id.buttonTitle);
+        subjectNameBtn.setText(name);
+        subjectNameBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Class cls = SubjectTeachingDepartment.class;
+                if (forAttendance) {
+                    cls = VolunteerAttendance.class;
+                }
+                Intent subjectDeptActivity = new Intent(TeachingDepartment.this, cls);
+                Bundle bundle = new Bundle();
+                bundle.putString("subjectId", id);
+                bundle.putString("subjectName", name);
+                subjectDeptActivity.putExtras(bundle);
+                startActivity(subjectDeptActivity);
+            }
+        });
+
+        TextView numTeachersTextView = (TextView) subjectButtonView.findViewById(R.id.buttonCaption);
+        numTeachersTextView.setText(numVolunteersLabel);
+
+        gridLayout.addView(subjectButtonView);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+            String name = data.getExtras().getString("name");
+            String id = data.getExtras().getString("id");
+            String numVolunteersLabel = "0 Teachers";
+
+            GridLayout gridLayout = (GridLayout) findViewById(R.id.gridWrap);
+            View subjectButtonView = getLayoutInflater().inflate(R.layout.subject_button, null);
+
+            initializeSubject(gridLayout, subjectButtonView, id, name, numVolunteersLabel);
         }
     }
 }
